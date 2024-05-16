b59df6cc699eedc7760bec4448a7b434f2cbc8dd
Chinemerem Okonkwo <engrchiok@gmail.com>
2024-05-16 20:43:12 +0100

    Completed task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,6 +3,6 @@ def print_last_digit(number):
     if number < 0:
         lstd = (-number) % 10
     else:
-        lstd= number % 10
+        lstd = number % 10
     print(f"{lstd}", end="")
     return lstd
