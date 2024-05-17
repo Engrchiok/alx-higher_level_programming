3b6a9ef28e02d3a3fb48670b7bb7a9571ed0d651
Chinemerem Okonkwo <engrchiok@gmail.com>
2024-05-17 12:43:40 +0100

    Completed task 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for a in range(1, 101):
-        if a % 3 == 0 and a % 5 == 0:
-            print("fizzbuzz ", end="")
-        elif a % 3 == 0:
-            print("fizz ", end="")
-        elif a % 5 == 0:
-            print("buzz ", end="")
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz ", end="")
+        elif number % 3 == 0:
+            print("Fizz ", end="")
+        elif number % 5 == 0:
+            print("Buzz ", end="")
         else:
-            print("{} ".format(a), end="")
+            print("{} ".format(number), end="")
