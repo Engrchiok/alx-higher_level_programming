71f90e7c3f022b5836319552049d7f4f2b912871
Chinemerem Okonkwo <engrchiok@gmail.com>
2024-05-17 14:55:28 +0100

    Completed task 15

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    cpy = str[:n] + str[n + 1:]
-    return cpy
+    if n < 0:
+        return str
+    else:
+        cpy = str[:n] + str[n + 1:]
+        return cpy
