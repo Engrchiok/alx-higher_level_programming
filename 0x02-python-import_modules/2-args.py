7d6495e48babecd0ba819441c181dc5d39771175
Chinemerem Okonkwo <engrchiok@gmail.com>
2024-06-04 18:17:03 +0100

    Done task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,5 +5,7 @@ if __name__ == "__main__":
         print(f"{len(sys.argv) - 1} {'argument' if len(sys.argv) == 2 else 'arguments'}:")
         for a in range(1, len(sys.argv)):
             print(f"{a}: {sys.argv[a]}")
-    if len(sys.argv) == 1:
+    elif len(sys.argv) == 1:
         print(f"{len(sys.argv) - 1} arguments.")
+    else:
+        pass
