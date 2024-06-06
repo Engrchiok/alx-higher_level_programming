dcebbdeea22f5f11edbd61fa26d70d01580cbf0a
Chinemerem Okonkwo <engrchiok@gmail.com>
2024-06-06 20:59:19 +0100

    Modified task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    """Python module that prints the number of and the list of its arguments."""
     import sys
     if len(sys.argv) >= 2:
         print(f"{len(sys.argv) - 1} {'argument' if len(sys.argv) == 2 else 'arguments'}:")
@@ -7,7 +8,3 @@ if __name__ == "__main__":
             print(f"{a}: {sys.argv[a]}")
     elif len(sys.argv) == 1:
         print(f"{len(sys.argv) - 1} arguments.")
-    else:
-        pass
-else:
-    pass
