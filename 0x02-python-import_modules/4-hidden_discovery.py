98394e33eeefa8e8dec3621027fa195cdf0b0911
Chinemerem Okonkwo <engrchiok@gmail.com>
2024-06-07 16:40:04 +0100

    Done task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Python module that prints all the names defined by the compiled module hidden_4.pyc"""
+    """Prints all the names defined by module hidden_4"""
     import hidden_4
 
     names = dir(hidden_4)
