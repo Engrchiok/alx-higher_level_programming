07c7af6c3ba0b6e67d8d929b0f8547cec5b78f10
Chinemerem Okonkwo <engrchiok@gmail.com>
2024-09-07 02:46:57 +0100

    Done task 1

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    ''' function that retrieves an index '''
+    if idx < 0:
+        return None
+    if idx >= (len(my_list) - 1):
+        return None
+    else:
+        return (my_list[idx])
