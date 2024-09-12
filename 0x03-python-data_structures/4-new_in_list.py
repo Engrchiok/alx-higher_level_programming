c8da6c50995b9254959b5fedbfd3f196538dd2b3
Chinemerem Okonkwo <engrchiok@gmail.com>
2024-09-12 12:09:08 +0100

    Done task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,6 +2,11 @@
 
 def new_in_list(my_list, idx, element):
     '''returns a copy of list replaced at an index'''
-    a = my_list[:]
-    a[idx] = element
-    return (a)
+    if idx < 0:
+        return (my_list)
+    elif idx >= len(my_list):
+        return (my_list)
+    else:
+        a = my_list[:]
+        a[idx] = element
+        return (a)
