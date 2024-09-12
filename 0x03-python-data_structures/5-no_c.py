3a5cfca366bcaea1c2417566d7c47520019f8353
Chinemerem Okonkwo <engrchiok@gmail.com>
2024-09-12 13:09:46 +0100

    Done task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
- #   '''removes all "c" and "C" characters'''
-    if my_string == None:
-        return
-    if type(my_string) != str:
-        return
+    '''removes all "c" and "C" characters'''
+ #   if my_string == None:
+  #      return
+   # if type(my_string) != str:
+    #    return
     a = ""
     for i in my_string:
         if i == 'c' or i == 'C':
