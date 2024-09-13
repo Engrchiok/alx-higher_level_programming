cc8004edbc10a68d76473fbaddb7528a506dcb59
Chinemerem Okonkwo <engrchiok@gmail.com>
2024-09-13 11:47:59 +0100

    Done task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,5 +3,5 @@
 def multiple_returns(sentence):
     '''returns tuple of string length and first chatacter of a sentence'''
     if sentence == None:
-        sentence[0] = None
+        sentence = ''
     return (len(sentence), sentence[0])
