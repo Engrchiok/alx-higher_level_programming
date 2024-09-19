55f3b5b028e2863d320b41fa907df677802b06e9
Chinemerem Okonkwo <engrchiok@gmail.com>
2024-09-19 22:50:31 +0100

    Done task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,11 +2,6 @@
 
 def best_score(a_dictionary):
     """ returns a key with the biggest integer value """
-#    if a_dictionary == None or a_dictionary == {}:
-#        return (None)
-#    elif a_dictionary.values() == None or a_dictionary.values() == []:
-#        return (None)
-#    else:
     if a_dictionary:
         a = int()
         for i in a_dictionary.keys():
