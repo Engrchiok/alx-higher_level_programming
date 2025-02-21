17c0bf6f004bc1b90db22eace0cc59563c8824e5
root <root@EMZ-DSK-224.>
2025-02-21 02:13:03 +0100

    Updated task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
             print(my_list[i], end='')
-        except Exception:
+        except IndexError:
             i -= 2
     print('')
     return int(i + 1)
