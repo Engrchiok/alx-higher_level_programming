53920c4c9bca0f75b3f25de83514b885bc902205
root <root@EMZ-DSK-224.>
2025-02-21 02:15:53 +0100

    Updated task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-
-def safe_print_list(my_list=[], x=0):
-    for i in range(x):
-        try:
-            print(my_list[i], end='')
-        except IndexError:
-            i -= 2
-    print('')
-    return int(i + 1)
+if __name__ == main:
+    def safe_print_list(my_list=[], x=0):
+        for i in range(x):
+            try:
+                print(my_list[i], end='')
+            except IndexError:
+                i -= 2
+        print('')
+        return int(i + 1)
