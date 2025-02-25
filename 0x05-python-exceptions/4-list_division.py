3a262722a6893e1ef5a03ee6d6c8e4af5197bf5c
Chinemerem Okonkwo <Engrchiok@gmail.com>
2025-02-26 00:05:47 +0100

    Updated task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -7,10 +7,13 @@ def list_division(my_list_1, my_list_2, list_length):
         except TypeError:
             print('wrong type')
             a.append(0)
+            continue
         except ZeroDivisionError:
             print('division by 0')
             a.append(0)
+            continue
         except IndexError:
             print('out of range')
             a.append(0)
+            break
     return a
