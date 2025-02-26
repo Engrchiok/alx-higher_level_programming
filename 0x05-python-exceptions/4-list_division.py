276277fb7146b3edd6b9ff673b13fd562a347c33
Chinemerem Okonkwo <Engrchiok@gmail.com>
2025-02-26 01:40:57 +0100

    Updated task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -13,7 +13,6 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print('out of range')
             b = 0
-            break
         finally:
             a.append(b)
     return a
