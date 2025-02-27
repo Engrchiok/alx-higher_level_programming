3c193636f1485ecffaf86bd8928a856fc9f6e2a5
root <root@EMZ-DSK-224.>
2025-02-27 12:43:37 +0100

    finished advanced tasksk 100

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,7 +2,7 @@
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except (ValueError, TypeError) as e:
+    except Exception as e:
         with open("/dev/stderr", "w") as f:
             f.write("Exception: {} \n".format(e))
         return False
