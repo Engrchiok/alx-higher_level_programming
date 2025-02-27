8cebf66745a6215439e4ccc071e7171186ce89da
root <root@EMZ-DSK-224.>
2025-02-27 13:02:15 +0100

    finished advanced tasksk 100

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
 import sys
-
 def safe_print_integer_err(value):
-    """
-    Prints an integer and returns True if successful.
-    Prints an error message to stderr and returns False if not an integer.
-    """
     try:
         print("{:d}".format(value))
         return True
     except Exception as e:
-        print("Exception: {}".format(e), file=sys.stderr)
+        print("Exception: {}".format(e), file = sys.stderr)
         return False
