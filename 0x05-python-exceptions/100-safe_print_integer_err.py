f10f4df75cc369aaee4168ff3a4de902ea7ddac8
root <root@EMZ-DSK-224.>
2025-02-27 10:45:34 +0100

    finished advanced tasksk 100

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,7 +3,7 @@ from sys import stderr as erm
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except Exception as e:
+    except ValueError as e:
         print('Exception: ', e, file = erm)
         return False
     else:
