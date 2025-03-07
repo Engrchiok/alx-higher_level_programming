4b511178b2eb54dcc8d183292bfe89e95353fdd6
Chinemerem Okonkwo <Engrchiok@gmail.com>
2025-03-08 00:47:05 +0100

    Done task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """
-This is a modole with an empty class that defines a ``Square``
+
+This is a modole with an empty class that defines a `Square`
 """
 
 class Square:
     """
+
     This is an empty class that defines a square
     """
     pass
