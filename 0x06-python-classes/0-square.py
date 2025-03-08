6dd10edce839d9d6152699ee67961d8992f69b6f
Chinemerem Okonkwo <Engrchiok@gmail.com>
2025-03-08 01:46:34 +0100

    Done task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""
+This is a module whose content is only a class.
+"""
+
 class Square:
+    """
+    This is a class that creates a square class object.
+    And, it is an empty class.
+    """
     pass
-    """
-    creates a square class object
-    """
+
