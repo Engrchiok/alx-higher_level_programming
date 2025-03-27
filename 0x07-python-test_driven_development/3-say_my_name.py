c996e97e124f88b1f148033cb3191c14f74f91b4
Chinemerem Okonkwo <Engrchiok@gmail.com>
2025-03-27 22:08:17 +0100

    Updated task 3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -5,10 +5,20 @@ first_name and last_name must be strings otherwise, raise a TypeError exception 
 """
 
 def say_my_name(first_name, last_name=""):
-    """."""
-    if type(first_name) is not str:
+    """Function that prints a given first name and last name.
+
+    args:
+        first_name (str): This is expected to be the first name.
+        last_name (str): This is expected to be the last name.
+
+    Raises:
+        TypeError: if first_name or last_name is not a string.
+
+    """
+
+    if type(first_name) is not str: #first_name must be a string
         raise TypeError("first_name must be a string")
-    elif type(last_name) is not str:
+    elif type(last_name) is not str: #last_name must be a string
         raise TypeError("last_name must be a string")
     else:
         print(f"My name is {first_name} {last_name}")
