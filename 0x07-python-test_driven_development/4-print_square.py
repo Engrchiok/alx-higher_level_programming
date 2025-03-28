b61d27515dde7ae872211bab7b501aa927077044
Chinemerem Okonkwo <Engrchiok@gmail.com>
2025-03-28 17:20:14 +0100

    Updated task 4

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -23,6 +23,7 @@ def print_square(*args):
     if len(args) != 1:
         raise ValueError("Only one argument is required")
     else:
+        size = args[0]
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
