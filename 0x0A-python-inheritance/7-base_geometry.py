6baf9d08e80ae311090e0909d9fcae1b5d1dc6db
Chinemerem Okonkwo <Engrchiok@gmail.com>
2025-05-02 17:01:45 +0100

    Done task 9

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""This module contains a class ``BaseGeometry`` that inherits from `object`
+class as its parent class."""
+
+
+class BaseGeometry(object):
+    """A class inheriting from `object` class as its parent class"""
+
+    def area(self):
+        """This is a class method.
+
+        Raises:
+            Exception: With message, ``area() is not implemented``.
+
+        """
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """This is a class method that validates `value`.
+
+        Args:
+            name: This is the name of the value object.
+            value: This is object value being validated.
+
+        Raises:
+            TypeError: if ``value`` is not an integer.
+            ValueError: if ``value`` is less than or equal to zero.
+
+        """
+
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+        else:
+            pass
