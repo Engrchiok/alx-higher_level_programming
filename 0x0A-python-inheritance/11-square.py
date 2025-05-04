1ad545ccc118cbbea3a64fa8f1ec22f9ac586327
Chinemerem Okonkwo <Engrchiok@gmail.com>
2025-05-05 00:09:11 +0100

    Done task 12

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -12,3 +12,6 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(self.__size, self.__size)
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
